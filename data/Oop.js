2b9e166692ca0430e8f16f4a06546165c18f7a40
ExoPolit <exopolit@gmx.de>
2024-01-07 16:35:41 +0100

    new push, added datastructure

---
+++ data/Oop.js
@@ -0,0 +1,9 @@
+const question = {
+    questionText: "What is the capital of France?",
+    answers: [
+        { text: "London", correct: false },
+        { text: "Paris", correct: true },
+        { text: "Berlin", correct: false },
+        { text: "Madrid", correct: false }
+    ]
+};
