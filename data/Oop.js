69a23c4f110de510bbdbebf9dc73ab83c98b5a46
ExoPolit <exopolit@gmx.de>
2024-01-07 17:12:29 +0100

    done OOP questions

--- data/Oop.js
+++ data/Oop.js
@@ -3,7 +3,7 @@ const questions = [
     {
         question: "What is Object-Oriented Programming (OOP) in JavaScript??",
         answers: [
-          {A: "A specific JavaScript data type", correct: flase},
+          {A: "A specific JavaScript data type", correct: false},
           {B: "A programming paradigm based on objects and classes", correct: true},
           {C: "A method for creating loops",correct: false},
           {D: "A JavaScript library for graphics",correct: false}
@@ -12,55 +12,55 @@ const questions = [
     {
         question: "In OOP, what is an object?",
         answers: [
-          {A: "A loop construct", correct: flase},
-          {B: "A special type of variable", correct: flase},
+          {A: "A loop construct", correct: false},
+          {B: "A special type of variable", correct: false},
           {C: "A function with a specific syntax",correct: false},
-          {D: "A collection of data and behavior",correct: true}
+          {D: "A collection of data and behavior",correct: true} // true
        ]
     },
     {
-        question: "What is Object-Oriented Programming (OOP) in JavaScript??",
+        question: "Which keyword is used to create a new instance of an object in JavaScript?",
         answers: [
-          {A: "A specific JavaScript data type", correct: flase},
-          {B: "A programming paradigm based on objects and classes", correct: flase},
-          {C: "A method for creating loops",correct: true},
-          {D: "A JavaScript library for graphics",correct: false}
+          {A: "`object`", correct: false},
+          {B: "`instance`", correct: false},
+          {C: "`new`",correct: true}, // true
+          {D: "`create`",correct: false}
        ]
     },
     {
-        question: "What is Object-Oriented Programming (OOP) in JavaScript??",
+        question: "What is a constructor in OOP?",
         answers: [
-          {A: "A specific JavaScript data type", correct: flase},
-          {B: "A programming paradigm based on objects and classes", correct: flase},
-          {C: "A method for creating loops",correct: true},
-          {D: "A JavaScript library for graphics",correct: false}
+          {A: "A method used to destroy objects", correct: false},
+          {B: "A function that creates and initializes objects", correct: true}, // true
+          {C: "A keyword for creating new properties in an objects",correct: false},
+          {D: "A built-in JavaScript class",correct: false}
        ]
     },
     {
-        question: "What is Object-Oriented Programming (OOP) in JavaScript??",
+        question: "What is encapsulation in OOP?",
         answers: [
-          {A: "A specific JavaScript data type", correct: flase},
-          {B: "A programming paradigm based on objects and classes", correct: flase},
-          {C: "A method for creating loops",correct: true},
-          {D: "A JavaScript library for graphics",correct: false}
+          {A: "The process of creating instances of objects", correct: false},
+          {B: "The bundling of data and the methods that operate on that data into a single unit", correct: true}, // true
+          {C: "The ability of an object to inherit properties from multiple classes",correct: false},
+          {D: "The concept of reusing code from one class in another class",correct: false}
        ]
     },
     {
-        question: "What is Object-Oriented Programming (OOP) in JavaScript??",
+        question: "What is inheritance in the context of Object-Oriented Programming (OOP) in JavaScript?",
         answers: [
-          {A: "A specific JavaScript data type", correct: flase},
-          {B: "A programming paradigm based on objects and classes", correct: flase},
-          {C: "A method for creating loops",correct: true},
-          {D: "A JavaScript library for graphics",correct: false}
+          {A: " The ability of an object to inherit properties from multiple classes", correct: false},
+          {B: "The process of creating new instances of objects", correct: false},
+          {C: "The bundling of data and methods into a single unit",correct: false},
+          {D: "The ability of a class to reuse properties and methods of another class",correct: true}, // true
        ]
     },
     {
-        question: "What is Object-Oriented Programming (OOP) in JavaScript??",
+        question: "What is polymorphism in the context of Object-Oriented Programming (OOP) in JavaScript?",
         answers: [
-          {A: "A specific JavaScript data type", correct: flase},
-          {B: "A programming paradigm based on objects and classes", correct: flase},
-          {C: "A method for creating loops",correct: true},
-          {D: "A JavaScript library for graphics",correct: false}
+          {A: "The ability of an object to inherit properties from multiple classes", correct: false},
+          {B: "The process of creating new instances of objects", correct: false},
+          {C: "The bundling of data and methods into a single unit",correct: false},
+          {D: "The ability of an object to take on multiple forms or types",correct: true}, // true
        ]
     },
       
