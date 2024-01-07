ec7ef3269736f66ee58688a4bd1418838535a3d7
ExoPolit <exopolit@gmx.de>
2024-01-07 16:59:55 +0100

    export default

--- data/Oop.js
+++ data/Oop.js
@@ -1,9 +1,70 @@
-const question = {
-    questionText: "What is the capital of France?",
-    answers: [
-        { text: "London", correct: false },
-        { text: "Paris", correct: true },
-        { text: "Berlin", correct: false },
-        { text: "Madrid", correct: false }
-    ]
-};
+
+const questions = [
+    {
+        question: "What is Object-Oriented Programming (OOP) in JavaScript??",
+        answers: [
+          {A: "A specific JavaScript data type", correct: flase},
+          {B: "A programming paradigm based on objects and classes", correct: true},
+          {C: "A method for creating loops",correct: false},
+          {D: "A JavaScript library for graphics",correct: false}
+       ]
+    },
+    {
+        question: "In OOP, what is an object?",
+        answers: [
+          {A: "A loop construct", correct: flase},
+          {B: "A special type of variable", correct: flase},
+          {C: "A function with a specific syntax",correct: false},
+          {D: "A collection of data and behavior",correct: true}
+       ]
+    },
+    {
+        question: "What is Object-Oriented Programming (OOP) in JavaScript??",
+        answers: [
+          {A: "A specific JavaScript data type", correct: flase},
+          {B: "A programming paradigm based on objects and classes", correct: flase},
+          {C: "A method for creating loops",correct: true},
+          {D: "A JavaScript library for graphics",correct: false}
+       ]
+    },
+    {
+        question: "What is Object-Oriented Programming (OOP) in JavaScript??",
+        answers: [
+          {A: "A specific JavaScript data type", correct: flase},
+          {B: "A programming paradigm based on objects and classes", correct: flase},
+          {C: "A method for creating loops",correct: true},
+          {D: "A JavaScript library for graphics",correct: false}
+       ]
+    },
+    {
+        question: "What is Object-Oriented Programming (OOP) in JavaScript??",
+        answers: [
+          {A: "A specific JavaScript data type", correct: flase},
+          {B: "A programming paradigm based on objects and classes", correct: flase},
+          {C: "A method for creating loops",correct: true},
+          {D: "A JavaScript library for graphics",correct: false}
+       ]
+    },
+    {
+        question: "What is Object-Oriented Programming (OOP) in JavaScript??",
+        answers: [
+          {A: "A specific JavaScript data type", correct: flase},
+          {B: "A programming paradigm based on objects and classes", correct: flase},
+          {C: "A method for creating loops",correct: true},
+          {D: "A JavaScript library for graphics",correct: false}
+       ]
+    },
+    {
+        question: "What is Object-Oriented Programming (OOP) in JavaScript??",
+        answers: [
+          {A: "A specific JavaScript data type", correct: flase},
+          {B: "A programming paradigm based on objects and classes", correct: flase},
+          {C: "A method for creating loops",correct: true},
+          {D: "A JavaScript library for graphics",correct: false}
+       ]
+    },
+      
+];
+
+
+export default questions;
