2b1bddd188885b615279dfb1a0c78e2822d40e33
ExoPolit <exopolit@gmx.de>
2024-01-11 21:20:04 +0100

    Update Banano logo size

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -11,7 +11,7 @@ const Nav = () => {
         
         <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container-fluid  fixed-top " style={{backgroundColor: 'var(--secondary-bg)'}}>
-                <img src={Banano} alt="banano" width={100} height={100} />
+                <img src={Banano} alt="banano" width={80} height={100} />
                 <h1 className="text-white">BananaScript</h1>
                 <div className="ml-auto" id="btn">
                     <Button variant="outline-none" style={{backgroundColor: 'var(--red-bg)', color: 'var(--font-wt)', fontWeight: '600'}} size='md' id="login-btn">LogIn</Button>
