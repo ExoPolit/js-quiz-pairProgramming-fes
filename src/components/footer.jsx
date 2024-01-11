affdb00037477bc9905425e22e0837c7d8fee907
ExoPolit <exopolit@gmx.de>
2024-01-11 21:15:56 +0100

    Add styles for footer and navigation buttons

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import '../styles/footer.css';
 const Footer = () => {
 
     return (
@@ -6,8 +7,8 @@ const Footer = () => {
       <footer className="navbar navbar-expand-lg navbar-light">
             <div className="container-fluid  fixed-bottom" style={{backgroundColor: 'var(--secondary-bg)', height: 100, justifyContent: "center", }}>
                 <div className="ml-auto" id="footer-content">
-                    <a href="#" className="text-white m-4" style={{fontSize:'var(--font-size-md', textDecoration: 'none'}}>About</a>
-                    <a href="#" className="text-white" style={{fontSize:'var(--font-size-md', textDecoration: 'none'}}>Contact</a>
+                    <a href="#" className="text-white m-4 "  style={{fontSize:'var(--font-size-md)', }}>About</a>
+                    <a href="#" className="text-white "  style={{fontSize:'var(--font-size-md)', }}>Contact</a>
                 </div>
             </div>
         </footer>
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import '../styles/nav.css';
 
 import Banano from "../assests/Banano.svg"
 
@@ -13,9 +13,9 @@ const Nav = () => {
             <div className="container-fluid  fixed-top " style={{backgroundColor: 'var(--secondary-bg)'}}>
                 <img src={Banano} alt="banano" width={100} height={100} />
                 <h1 className="text-white">BananaScript</h1>
-                <div className="ml-auto">
-                    <Button variant="outline-none" style={{backgroundColor: 'var(--red-bg)', color: 'var(--font-wt)', fontWeight: '600'}} size='md' className='m-2 overflow-hidden'>LogIn</Button>
-                    <Button variant="outline-none" style={{backgroundColor: 'var(--red-bg)', color: 'var(--font-wt)', fontWeight: '600'}} size='md'>SignUp</Button>
+                <div className="ml-auto" id="btn">
+                    <Button variant="outline-none" style={{backgroundColor: 'var(--red-bg)', color: 'var(--font-wt)', fontWeight: '600'}} size='md' id="login-btn">LogIn</Button>
+                    <Button variant="outline-none" style={{backgroundColor: 'var(--red-bg)', color: 'var(--font-wt)', fontWeight: '600'}} size='md' id="signout-btn">SignUp</Button>
                     
                 </div>
             </div>
