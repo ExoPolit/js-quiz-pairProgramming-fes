d10899a6a8caac25f738b8e57e59216e9902626a
ExoPolit <exopolit@gmx.de>
2024-01-11 19:29:49 +0100

    footer set

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,27 +1,15 @@
 import React from "react";
-
 const Footer = () => {
 
     return (
         <>
-        <footer className="">
-            <div className="container-fluid bg-dark text-light fixed-bottom">
-                <div className="row">
-                    <div className="col-10 mx-auto">
-                        <div className="row">
-                            <div className="col-md-4 col-12">
-                                <h6 className="text-center" style={{fontSize: 'var(--font-size-md)' }}>Why did the banana go to the doctor?</h6>
-                                <p className="text-center" style={{fontSize: 'var(--font-size-sm)'}}>
-                                Bananaville hosted a Banana Comedy Festival, uniting bananas in laughter. Even the apples couldn't resist. A night where bananas showed they're not just fruits but comedic legends.
-                                </p>
-                            </div>
-                            <div className="col-md-4 col-12">
-                                <h1 className="text-center" >Contact Us</h1>
-                             </div>
-                             </div>
-                             </div>
-                             </div>
-                                </div>
+      <footer className="navbar navbar-expand-lg navbar-light">
+            <div className="container-fluid  fixed-bottom" style={{backgroundColor: 'var(--secondary-bg)', height: 100, justifyContent: "center", }}>
+                <div className="ml-auto" id="footer-content">
+                    <a href="#" className="text-white m-4" style={{fontSize:'var(--font-size-md', textDecoration: 'none'}}>About</a>
+                    <a href="#" className="text-white" style={{fontSize:'var(--font-size-md', textDecoration: 'none'}}>Contact</a>
+                </div>
+            </div>
         </footer>
 
         </>
