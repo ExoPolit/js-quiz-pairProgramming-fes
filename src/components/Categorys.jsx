e8382a91ecfc7ce1de2c6b114f2dadae54522640
ExoPolit <exopolit@gmx.de>
2024-01-13 17:26:36 +0100

    Refactor Categorys component styling

--- src/components/Categorys.jsx
+++ src/components/Categorys.jsx
@@ -7,15 +7,15 @@ import categories from '../data/CategoryData';
 const Categorys = () => {
   
   return (
-    <div style={{backgroundColor: 'var(--main-bg)', padding: '50px'}}>
-    <div className="container-lg min-height-100">
+    <div style={{backgroundColor: 'var(--main-bg)', padding: '25px'}}>
+    <div className="container-lg">
         <div className="row">
             {categories.map((categories, index) => (
-            <div key={index} className="col-lg-3 col-md-4 col-md-6 mb-4">
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title" style={{textAlign: 'center'}}>{categories}</h5>
-                        <Button variant="outline-none" style={{backgroundColor: 'var(--red-bg)', color: 'var(--font-wt)', fontWeight: '600', width: '100%'}} size='md' id="signout-btn">Start</Button>
+            <div key={index} className="col-lg-3 col-md-3 col-md-6 mb-4">
+                <div className="card" >
+                    <div className="card-body" style={{placeItems:'center', textAlign:'center'}}>
+                        <h5 className="card-title">{categories}</h5>
+                        <Button variant="outline-none" style={{backgroundColor: 'var(--red-bg)', color: 'var(--font-wt)', fontWeight: '600', width:'100%'}} size='md' id="signout-btn">Start</Button>
                     </div>
                 </div>
             </div>
