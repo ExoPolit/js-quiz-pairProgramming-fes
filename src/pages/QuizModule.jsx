3e46949903ee3f0d1c395285442c2fa6a8919f14
SebZG <sebastian.zapata.g@gmail.com>
2024-01-13 18:48:07 +0000

    Trying to get categories to show dynamically in QuizModule.jsx

--- src/pages/QuizModule.jsx
+++ src/pages/QuizModule.jsx
@@ -1,8 +1,29 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import categories from "../data/CategoryData";
 
 const QuizModule = () => {
 	const { cat } = useParams();
-  
-	return <div>{cat}</div>;
+	const [category, setCategory] = useState([]);
+
+
+	useEffect(() => {
+		setCategory(categories);
+		console.log(category);
+		console.log(categories);
+		console.log(categories.cat);
+	}, []);
+
+	return (
+		<>
+			<h1>Category: {cat}</h1>
+			<h2>Q1: {}</h2>
+			<p>A. {}</p>
+			<p>B. {}</p>
+			<p>C. {}</p>
+			<p>D. {}</p>
+		</>
+	);
 };
 export default QuizModule;
