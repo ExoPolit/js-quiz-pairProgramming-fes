61fbe10b02368a6cd7b77d1b44caaaa05bc353a4
SebZG <sebastian.zapata.g@gmail.com>
2024-01-13 16:22:06 +0000

    Formated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,35 +4,31 @@ import Categorys from "../components/Categorys";
 import { useState } from "react";
 
 const Home = () => {
-  const [show, setShow] = useState(false);
+	const [show, setShow] = useState(false);
 
-  const toggleCategories = () => {
-    setShow(!show);
-  };
- 
+	const toggleCategories = () => {
+		setShow(!show);
+	};
 
-
-  return (
-    <>
-    {!show && <Nav/>}
-      <div
-        style={{
-            overflow: 'hidden',
-          maxHeight: show ? "100vh" : "0",
-          transition: "max-height 1.5s",
-        }}
-        >
-        {show && <Categorys />}
-      </div>
-      {!show && <Footer />}
-      <button
-        onClick={toggleCategories}
-        style={{ position: "absolute", top: "25rem" }}
-      >
-        Show More
-      </button>
-    </>
-  );
+	return (
+		<>
+			{!show && <Nav />}
+			<div
+				style={{
+					overflow: "hidden",
+					maxHeight: show ? "100vh" : "0",
+					transition: "max-height 1.5s",
+				}}>
+				{show && <Categorys />}
+			</div>
+			{!show && <Footer />}
+			<button
+				onClick={toggleCategories}
+				style={{ position: "absolute", top: "25rem" }}>
+				Show More
+			</button>
+		</>
+	);
 };
 
 export default Home;
