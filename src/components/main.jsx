88ad3dee49c2c8f0a6f7f4079cf1652d7574e6be
ExoPolit <exopolit@gmx.de>
2024-01-14 15:18:55 +0100

    Update main component layout and button styles

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -28,26 +28,27 @@ const MainModule = ({toggleCategories}) => {
         style={{
           backgroundColor: "var(--main-bg)",
           height: "27.375rem",
-          width: "53.063rem",
+          width: "60rem",
+          borderRadius: "255px",
         }}
       >
         <h1>Modes</h1>
         <div className="row">
         <div className="d-flex justify-content-center">
-        <div className="col-8">
+        <div className="col-4">
             <Button className="p-2" onClick={toggleCategories}> Classic Mode </Button>
           </div>
-          <div className="col-8">
+          <div className="col-4">
             <Button className="p-2" onClick={toggleCategories}> Practice Mode </Button>
           </div>
           </div>
         </div>
         <div className="row">
         <div className="d-flex justify-content-center">
-          <div className="col-8">
+          <div className="col-4">
             <Button className="p-2" onClick={toggleCategories}> Practice Mode </Button>
           </div>
-          <div className="col-8">
+          <div className="col-4">
             <Button className="p-2" onClick={toggleCategories}> Practice Mode </Button>
           </div>
         </div>
