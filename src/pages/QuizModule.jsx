8c076aae60bab52fc1e15b46258faa2d3427911c
SebZG <sebastian.zapata.g@gmail.com>
2024-01-14 14:01:35 +0000

    QuizModule design started

--- src/pages/QuizModule.jsx
+++ src/pages/QuizModule.jsx
@@ -7,6 +7,9 @@ import categories from "../data/CategoryData";
 
 import { Button } from "react-bootstrap";
 
+import Nav from "../components/nav";
+import Footer from "../components/footer";
+
 const QuizModule = () => {
 	const { cat } = useParams();
 	const [questions, setQuestions] = useState([]);
@@ -42,55 +45,65 @@ const QuizModule = () => {
 
 	return (
 		<>
-			<h1>Category: {cat}</h1>
-			{questions.slice(current, current + 1).map((question, index) => (
-				<div key={index}>
-					<h2>{`Q${index + (current + 1)}: ${question.question}`}</h2>
-					{question.answers.map((answer, ansIndex) => (
-						<button
-							key={ansIndex}
-							className=" w-100 text-start btn-answers"
-							variant="outline-none">
-							<p className="option-left">{Object.keys(answer)[0]}.</p>{" "}
-							<p className="option-right">{answer[Object.keys(answer)[0]]}</p>
-						</button>
+			<Nav cat={cat} />
+			<div id="quizModule" className="container">
+				<div className="row">
+					{/* <h1>Category: {cat}</h1> */}
+					{questions.slice(current, current + 1).map((question, index) => (
+						<div key={index}>
+							<h2>{`Q${index + (current + 1)}: ${question.question}`}</h2>
+							{question.answers.map((answer, ansIndex) => (
+								<Button
+									key={ansIndex}
+									className="w-100 text-start btn-answers"
+									variant="outline-none">
+									<p className="option-left">{Object.keys(answer)[0]}.</p>{" "}
+									<p className="option-right">
+										{answer[Object.keys(answer)[0]]}
+									</p>
+								</Button>
+							))}
+						</div>
 					))}
 				</div>
-			))}
-			{current === 0 ? (
-				<Button
-					className="col-12 back-next"
-					variant="outline-none"
-					onClick={handleNext}
-					size={btnSize}>
-					Next
-				</Button>
-			) : current > 0 && current < questions.length - 1 ? (
-				<>
+			</div>
+			<div className="btn-wrapper">
+				{current === 0 ? (
 					<Button
-						className="col-6 back-next"
-						variant="outline-none"
-						onClick={handleBack}
-						size={btnSize}>
-						Back
-					</Button>
-					<Button
-						className="col-6 back-next"
+						className="col-12 back-next"
 						variant="outline-none"
 						onClick={handleNext}
 						size={btnSize}>
 						Next
 					</Button>
-				</>
-			) : (
-				<Button
-					className="col-12 back-next"
-					variant="outline-none"
-					onClick={handleBack}
-					size={btnSize}>
-					Back
-				</Button>
-			)}
+				) : current > 0 && current < questions.length - 1 ? (
+					<>
+						<Button
+							className="col-6 back-next"
+							variant="outline-none"
+							onClick={handleBack}
+							size={btnSize}>
+							Back
+						</Button>
+						<Button
+							className="col-6 back-next"
+							variant="outline-none"
+							onClick={handleNext}
+							size={btnSize}>
+							Next
+						</Button>
+					</>
+				) : (
+					<Button
+						className="col-12 back-next"
+						variant="outline-none"
+						onClick={handleBack}
+						size={btnSize}>
+						Back
+					</Button>
+				)}
+			</div>
+			<Footer />
 		</>
 	);
 };
