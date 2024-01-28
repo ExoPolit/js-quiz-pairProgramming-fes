99c2bc3f78f846d1724f1555f000014fa34f295e
ExoPolit <exopolit@gmx.de>
2024-01-28 12:45:50 +0100

    Refactor ClassicPage component and add useEffect hook

--- src/pages/ClassicPage.jsx
+++ src/pages/ClassicPage.jsx
@@ -1,44 +1,48 @@
-import React,{ useState  } from "react"
-import {Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import categories from "../data/CategoryData";
 
 const ClassicPage = () => {
-    const[classicQuestions, setClassicQuestions] = useState([getRandomQuestion()]);
+  const [classicQuestions, setClassicQuestions] = useState([]);
 
-    function getRandomQuestion(){
-        const allQuestions = categories.flatMap(category => Object.values(category)[0]);
-        const shuffledQuestions = shuffleArray(allQuestions);
-        return shuffledQuestions.slice(0, count);
+  useEffect(() => {
+    setClassicQuestions(getRandomQuestions(40));
+  }, []);
+
+  function getRandomQuestions(count) {
+    const allQuestions = categories.flatMap(category => Object.values(category)[0]);
+    const shuffledQuestions = shuffleArray(allQuestions);
+    return shuffledQuestions.slice(0, count);
+  }
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
+    return array;
+  }
 
-    function shuffleArray(array) {
-        for(let i = array.length - 1; i > 0; i--){
-            const j = Math.floor(Math.random() * (i+1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
-
-    return (
-        <div>
-            <h2>Classic Mode</h2>
-            {classicQuestions.map((question, index) => (
-                <div key={index}>
-                    <h3>
-                        {`Q${index + 1}: ${question.question}`}
-                    </h3>
-                    {question.answers.map((answer, index) => (
-                        <p key={index}>{answer.text}</p>
-                        
-                        ))}
-                        </div>
+  return (
+    <div>
+    <h2>Classic Mode</h2>
+    {classicQuestions.map((question, index) => (
+      <div key={index}>
+        <h3>{`Q${index + 1}: ${question.question || ""}`}</h3>
+        {question.answers && (
+          <ul>
+            {Object.entries(question.answers).map(([key, value], ansIndex) => (
+              <li key={ansIndex}>{`${key}: ${value}`}</li>
             ))}
-            <Link to="/">
-                <button>Home</button>
-            </Link>
-        </div>
-                        
-    )
-}
+          </ul>
+        )}
+      </div>
+    ))}
+    <Link to="/">
+      <button>Home</button>
+    </Link>
+  </div>
+);
+};
 
 export default ClassicPage;
